1e2d50025614da6254730025464c727313107ebd
ignatiousanto <ignatiousantop@gmail.com>
2017-06-27 16:35:06 +0530

    Softmax tutorial
    addirtion of tutorial for softmax on tensor flow

--- tf.py
+++ tf.py
@@ -2,81 +2,34 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import tensorflow as tf
 import numpy as np
-node1 = tf.constant([3.0])
-node2 = tf.constant([4.0])
-node3 = node1 + node2
-node4 = tf.add(node1,node3)
-sess = tf.Session()
 
-"""print(50*"*-")
-print (sess.run(node3*node4))
-print (node1,node4)
+###softmax regression
 
-a = tf.placeholder(tf.float32)
-b = tf.placeholder(tf.float32)
-
-adder_node = a+b
-mulp_node = a*b
-
-print (sess.run(adder_node,{a:[3.0,3.0],b:[4.0,5.0]}))
-print (sess.run(mulp_node,{a:3,b:5}))"""
-
-M = tf.Variable([.3], dtype= tf.float32)
-c = tf.Variable([-.3], dtype= tf.float32)
-x = tf.placeholder(tf.float32)
-y = tf.placeholder(tf.float32)
-
-linear_model = M * x + c
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 
+x = tf.placeholder(tf.float32, [None, 784])
+y_ = tf.placeholder(tf.float32, [None,10])
 
-loss = tf.reduce_sum(tf.square(linear_model - y))
-optimizer = tf.train.GradientDescentOptimizer(0.01)
+W = tf.Variable(tf.zeros([784,10]))
+b = tf.Variable(tf.zeros([10]))
 
-x_train = [1,2,3,4]
-y_train = [0,-1,-2,-3]
-x_eval = np.array([2., 5., 8., 1.])
-y_eval = np.array([-1.01, -4.1, -7, 0.])
+y = tf.nn.softmax(tf.matmul(x, W) + b)
 
-train = optimizer.minimize(loss)
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices = [1]))
+train_step = tf.train.GradientDescentOptimizer(0.05).minimize(cross_entropy)
 
-init = tf.global_variables_initializer()
-sess.run(init)
+sess = tf.InteractiveSession()
+tf.global_variables_initializer().run()
 
-for i in range(1000):
-	sess.run(train,{x:x_train, y:y_train})
+for _ in range(1000):
+  batch_xs, batch_ys = mnist.train.next_batch(100)
+  sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
-print (sess.run([M,c]))
-
-##eval
-
-print(sess.run(linear_model, {x:x_eval}))
-print(sess.run(loss, {x:x_eval, y:y_eval}))
-print (y_eval)
-
-
-
-
-
-
-####using the built in logistic regressor modle with contrib.learn
-"""
-feat = [tf.contrib.layers.real_valued_columns("x", dimension = 1)]
-
-estimator = tf.contrib.learn.LinearRegressor(feature_columns = feat)
-x_tr = np.array([1., 2., 3., 4.])
-y_tr = np.array([0., -1., -2., -3.])
-x_eval = np.array([2., 5., 8., 1.])
-y_eval = np.array([-1.01, -4.1, -7, 0.])
-
-input_fn = tf.contrib.learn.io.numpy_input_fn({"x" :x_tr, y_tr,batch})
-
-
-"""
-
-
-
-
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
 
 
 
